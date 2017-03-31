a623539a6fb2d0d8685af2bdb390f37fd70a114b
Kanybek <kano.momuke@gmail.com>
2017-03-31 11:49:25 +0600

    1 worker channel added.

---
+++ grpc/multithread/worker_ex2.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	_ "expvar"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+	"errors"
+)
+
+type job struct {
+	name     string
+	duration time.Duration
+}
+
+func listenForJobReceive()  {
+	for j := range jobsChannel {
+		doWork(1, j)
+	}
+}
+
+func doWork(id int, j *job) {
+	fmt.Printf("worker%d: started %s, working for %f seconds\n", id, j.name, j.duration.Seconds())
+	time.Sleep(j.duration)
+	fmt.Printf("worker%d: completed %s!\n", id, j.name)
+
+	if j.duration < 4000000000 {
+		go func() {
+			jobsFinishedCahnnel <- j
+		}()
+	} else {
+		go func() {
+			err := errors.New("Not found")
+			errorChannel <- err
+		}()
+	}
+}
+
+func requestHandler(jobs chan *job, w http.ResponseWriter, r *http.Request) (*job, error) {
+
+	job := &job{"some job", 5000000000}
+	go func() {
+		fmt.Printf("added: %s %s\n", job.name, job.duration)
+		jobs <- job
+	}()
+
+	w.WriteHeader(http.StatusCreated)
+
+	select {
+	case jobFinished := <-jobsFinishedCahnnel:
+		return jobFinished, nil
+	case error := <-errorChannel:
+		return nil, error
+	}
+}
+
+var jobsChannel chan *job
+var jobsFinishedCahnnel chan *job
+var errorChannel chan error
+
+func main() {
+
+	var (
+		maxQueueSize = flag.Int("max_queue_size", 10000, "The size of job queue")
+		port         = flag.String("port", "8080", "The server port")
+	)
+	flag.Parse()
+
+	jobsChannel = make(chan *job, *maxQueueSize)
+	jobsFinishedCahnnel = make(chan *job, *maxQueueSize)
+	errorChannel = make(chan error, *maxQueueSize)
+
+	go listenForJobReceive()
+
+	http.HandleFunc("/work", func(w http.ResponseWriter, r *http.Request) {
+		res, err := requestHandler(jobsChannel, w, r)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if res != nil {
+			fmt.Printf("worker: totallly completed %s!\n", res.name)
+		}
+	})
+	
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
