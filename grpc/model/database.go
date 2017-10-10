3ecd9655eb177631cf51a9c88f5ac385131ba382
Kanybek <kano.momuke@gmail.com>
2017-10-10 13:02:14 +0600

    fix in database

--- grpc/model/database.go
+++ grpc/model/database.go
@@ -21,8 +21,8 @@ func NewDB(dataSourceName string) (*sqlx.DB, error) {
 	)
 
 	fmt.Println(connInfo)
-	//db, err := sqlx.Connect("postgres", connInfo) // for compose
-	db, err := sqlx.Connect("postgres", "user=kanybek dbname=databasename password=nazgulum host=localhost port=5432 sslmode=disable")
+	db, err := sqlx.Connect("postgres", connInfo) // for compose
+	//db, err := sqlx.Connect("postgres", "user=kanybek dbname=databasename password=nazgulum host=localhost port=5432 sslmode=disable")
 	if err != nil {
 		log.WithFields(log.Fields{
 			"connection info": connInfo,
